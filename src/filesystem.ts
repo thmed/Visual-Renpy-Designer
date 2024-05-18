b01a539316ff76e142c8f0e69d95047b5a581f64
QuftCuty <cmftman@outlook.com>
2024-05-19 02:45:05 +0800

    Update filesystem.ts and project_plan.drawio

--- src/filesystem.ts
+++ src/filesystem.ts
@@ -1,9 +1,15 @@
-export { FileSystem };
-
+export {FileSystem};
 
 class FileSystem {
     
-    static jsonPath: string;
+    /*Static member attribute*/
+    static user_files:undefined;//用户文件存储结构（可能是个对象或json）
+
+    /*Static member function*/
+    static readUserFiles():void{//读取用户文件并存于一定的文件存储结构中,包括renpy script文件，资源文件等
+
+    }
+
+    /*Static member object*/
 
 }
-
