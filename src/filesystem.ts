38396cb89aa97c2818998f5990042c1ed69dc37c
QuftCuty <cmftman@outlook.com>
2024-05-21 00:37:27 +0800

    Update filesystem.ts and something else. Learning fs module of nodejs.

--- src/filesystem.ts
+++ src/filesystem.ts
@@ -1,15 +1,46 @@
 export {FileSystem};
 
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
+
 class FileSystem {
     
     /*Static member attribute*/
-    static user_files:undefined;//用户文件存储结构（可能是个对象或json）
+    static workspace_folder = this.getWorkspace();//工作区文件夹路径
+    static gamecontent_json_exit:Boolean;//记录工作区game文件下是否存在游戏内容json文件
+    static gamecontent_json_path:string | string[];//记录游戏内容json文件的路径
 
     /*Static member function*/
-    static readUserFiles():void{//读取用户文件并存于一定的文件存储结构中,包括renpy script文件，资源文件等
+    static readUserFilesStructrue():void{//读取用户文件并存于一定的文件存储结构中,包括renpy script文件，资源文件等
 
     }
 
-    /*Static member object*/
+    createGameContentJson():void{//在game文件夹下创建记录游戏内容的json文件
 
+    }
+
+    static readGameContentSentence():void{//读取选定sentence的内容，参数：选定的sentence
+
+    }
+    
+    static detectGameContentJson():boolean | string | string[]{//检查用户工作区是否存在游戏内容json文件，存在则返回路径（可以是一个路径，也可以是多个路径返回数组），不存在则返回false
+        return false;
+    }
+
+    static getWorkspace(){//获取用户工作区文件夹路径，没有打开工作区则设置一个标志变量为0（暂时没写设置）
+        let temp_workspace = vscode.workspace.workspaceFolders;
+        if (temp_workspace && temp_workspace.length > 0) {
+            const workspaceFolder = temp_workspace[0].uri.fsPath;
+            return workspaceFolder;
+        } else {
+            vscode.window.showInformationMessage('No workspace folder open');
+            return undefined;
+        }
+    }
+
+    /*Static member object*/
+    GameContentChapter:object = {
+
+    };
 }
---
+++ src/webview/communication.js
@@ -0,0 +1,13 @@
+//这个文件暂时用来声明可能会存在的通信相关函数
+
+function requestChapterList(){//请求一个记录chapter的对象结构数据，参数：游戏内容文件名称
+
+}
+
+function requestSectionList(){//请求一个记录section的对象结构数据，参数：游戏内容文件名称，chapter名称
+
+}
+
+function requestSentenceList(){//请求一个记录了sentence以及其简略信息的对象结构数据，参数：游戏内容名称，chapter名称，section名称
+
+}
