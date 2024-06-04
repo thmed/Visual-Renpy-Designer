274770372ab1e4017812915e0d954529f8fde15a
CMFTman <CMFTman@github.com>
2024-06-05 00:06:39 +0800

    Update message structure and some interaction function

--- src/filesystem.ts
+++ src/filesystem.ts
@@ -18,14 +18,15 @@ import * as jsonstream from 'jsonstream';
 class FileSystem {
 
     /*************************************************Static member attribute*********************************************************/
+    static fileRegex = /\.vrd\.json$/;//游戏内容json文件正则表达式
 
     static workspace_folder:string | undefined;//工作区文件夹路径
     static game_folder:string | boolean;//game文件夹路径
     static gamecontent_json_path:string[] | boolean;//记录游戏内容json文件的路径
 
-    static current_selected_json:string;//当前选择的json文件
-    static current_selected_chapter:string;//当前选择的章
-    static current_selected_section:string;//当前选择的节
+    static current_selected_json:string;//当前所处的json文件
+    static current_selected_chapter:string;//当前所处的章
+    static current_selected_section:string;//当前所处的节
 
     /***********************************************Static initialization method******************************************************/
 
@@ -45,48 +46,52 @@ class FileSystem {
 
 
 
-    static getJsonList(){//获取游戏内容json文件列表
 
-    }
 
-    static async getChapterList(json_path: string[]): Promise<string[]> {//获取章列表
-        const chapterNames: string[] = [];
-      
-        return new Promise((resolve, reject) => {
-          const readStream = fs.createReadStream(json_path[0]);
-          const jsonStream = jsonstream.parse('chapter.*.chaptername');
-      
-          readStream.pipe(jsonStream);
-      
-          jsonStream.on('data', (chaptername: string) => {
-            chapterNames.push(chaptername);
-          });
-      
-          jsonStream.on('end', () => {
-            console.log('File reading and parsing completed.');
-            resolve(chapterNames); // 解析完成后返回结果
-          });
-      
-          jsonStream.on('error', (err) => {
-            console.error('Error reading or parsing file:', err);
-            reject(err); // 处理错误
-          });
-        });
-    }
 
     
-    static getSectionList(){//获取节列表
+
+    static setCurrentSelectedJson():void{//设置当前所处的json文件
 
     }
 
-    static getSentenceList(){//获取句子列表
+    static setCurrentSelectedChapter():void{//设置当前所处的章
 
     }
 
-    static getGameContentSentence(): void {//读取选定sentence的内容，参数：选定的sentence
+    static setCurrentSelectedSection():void{//设置当前所处的节
 
     }
 
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+    static getGameContentJsonList():string[]{//获取游戏内容json文件列表
+        const fileRegex = /[^\\]+$/;//游戏内容json文件正则表达式
+        let filteredStrings:string[]=[];
+        if(Array.isArray(this.gamecontent_json_path)){
+            for(let path of this.gamecontent_json_path){
+                let match = path.match(fileRegex);
+                if(match){
+                    filteredStrings.push(match[0]);
+                }
+            }
+        }
+        return filteredStrings;
+    }
+
+    //待修改
     static createGameContentJson(): void {//在game文件夹下创建记录游戏内容的json文件
         if(typeof this.game_folder === 'string'){
             let filePath = path.join(this.game_folder,'gamecontent.vrd.json');
@@ -97,6 +102,90 @@ class FileSystem {
                 console.error('Error creating file:', err);
             }
         }
+    }
+
+    static deleteGameContentJson():void{//删除指定的游戏内容json文件
+
+    }
+
+
+
+
+
+    static async getChapterList(json_path: string): Promise<string[]> {//获取章列表
+        const chapterNames: string[] = [];
+      
+        return new Promise((resolve, reject) => {
+            if(typeof this.game_folder==='string'){
+                const readStream = fs.createReadStream(path.join(this.game_folder,json_path));
+                const jsonStream = jsonstream.parse('chapter.*.chaptername');
+      
+                readStream.pipe(jsonStream);
+            
+                jsonStream.on('data', (chaptername: string) => {
+                  chapterNames.push(chaptername);
+                });
+            
+                jsonStream.on('end', () => {
+                  console.log('File reading and parsing completed.');
+                  resolve(chapterNames); // 解析完成后返回结果
+                });
+            
+                jsonStream.on('error', (err) => {
+                  console.error('Error reading or parsing file:', err);
+                  reject(err); // 处理错误
+                });
+            }
+
+        });
+    }
+
+    static createChapter():void{//在当前json文件下新建一个章
+
+    }
+
+    static deleteChapter():void{//删除当前json文件下的一个章
+
+    }
+
+    static changeChapterOrder():void{//改变章顺序
+
+    }
+    
+
+
+
+
+
+
+    static getSectionList(){//获取节列表
+
+    }
+
+    static createSection():void{//在当前章下新建一个节
+
+    }
+
+    static deleteSection():void{//删除当前章下的一个节
+
+    }
+
+    static changeSectionOrder():void{//改变节顺序
+
+    }
+
+
+
+
+
+
+
+    static getSentenceList(){//获取当前节下的句子列表
+
+    }
+
+    static getSentenceContent(): void {//读取当前节下选定sentence的内容，参数：选定的sentence
+
     }
 
 
@@ -145,8 +234,7 @@ class FileSystem {
         if (typeof this.game_folder === 'string') {
             try {
                 const files = fs.readdirSync(this.game_folder);
-                const fileRegex = /\.vrd\.json$/;//游戏内容json文件正则表达式
-                let gamejcontent_json = files.filter(file => fileRegex.test(file));
+                let gamejcontent_json = files.filter(file => this.fileRegex.test(file));
                 if (gamejcontent_json.length > 0) {
                     return gamejcontent_json.map(file => path.join(this.game_folder as string, file));
                 }
